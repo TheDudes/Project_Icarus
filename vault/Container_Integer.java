e5a665475e1faf3debb9a7e0333e257d59369891
Vault <s-aweinb@fh-landshut.de>
2014-02-14 16:25:50 +0000

    Changed
    Container_Long
    Container_Integer 
    
    classes

--- vault/Container_Integer.java
+++ vault/Container_Integer.java
@@ -5,24 +5,21 @@ import java.util.Map;
 
 public class Container_Integer {
     
-    Map<String, Integer> integer = new HashMap<>();
+    Map<String, Integer> container_integer = new HashMap<>();
     
-    
-    //Adds a new Variable to the Map//
     public void put(String varName, int varValue){
-        integer.put(varName, varValue);
+        container_integer.put(varName, varValue);
     }
-    //Changes the Value of the Variable//
     public void set(String varName, int varValue){
-        integer.put(varName, varValue);
+        container_integer.put(varName, varValue);
     }
-    //Gives back the value of the variable// 
-    //Throws Exception if variable is not in Map//
+    public boolean is_In(String varName){
+        return container_integer.containsKey(varName);
+    }
     public int getValue(String varName) throws Exception { 
         int value = 0;
-        
-        if(integer.containsKey(varName)){
-            for(Map.Entry<String, Integer> item: integer.entrySet()){ 
+        if(container_integer.containsKey(varName)){
+            for(Map.Entry<String, Integer> item: container_integer.entrySet()){ 
                 if(item.getKey().equals(varName)){
                     value = item.getValue();
                 } 
@@ -31,14 +28,6 @@ public class Container_Integer {
         } else {
             throw new Exception("Variable \"" + varName + "\" might not be initialised");
         }
-    }
-    //Checks wether variable is in Map - true ->> 1 - false ->> 0//
-    public boolean is_In(String varName){
-        boolean in = false;
-        if(integer.containsKey(varName)){
-            in = true;
-        }
-        return in;
     }
 
 }
