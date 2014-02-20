957a6b36fc37d5e02000e8cbb9d46640c7455f51
d4ryus <w.wackerbauer@yahoo.de>
2014-02-20 13:01:57 +0000

    formatted code, replaces tabs with spaces etc

--- d4ryus/Boolean_Stack.java
+++ d4ryus/Boolean_Stack.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
  */
 
 public class Boolean_Stack {
-    
-    /* 
-	 * ArrayList containing the Elements of the Stack, basically the heart of the class,
+
+    /**
+     * ArrayList containing the Elements of the Stack, basically the heart of the class,
      * contains Strings
-     */ 
+     */
     private ArrayList<Boolean> container = new ArrayList<Boolean>();
-    
+
     /*
      * stores the element in the Stack/Array
      * @param toStore the element to be stored in the Stack/Array
@@ -19,11 +19,11 @@ public class Boolean_Stack {
     public void push(Boolean toStore){
         container.add(0, toStore);
     }
-    
+
     public Boolean get_first(){
-		return (container.get(0));
+        return (container.get(0));
     }
-    
+
     public void pop(){
         container.remove(0);
     }
--- d4ryus/Icarus.java
+++ d4ryus/Icarus.java
@@ -4,22 +4,22 @@
  * Pre Alpha Icarus Interpreter Version 0.2
  *
  * current status:
- *	- able to preprozess file (removes comments and stuff, no one needs that, right?)
- *	- able to interpret simple VAR blocks (dynamic allocation with HashMaps, l33th4XX!)
- *	- able to Print out stuff (yeah yeah i know, Structured Text has no PRINT function,
- *							   and thats one of the many reasons why this Interpreter 
- *							   is fucking awesome! why? cause it can interpret more than
- *							   it should or could!)
- *	- able to interpret IF conditions (pretty damn great, isnt it?)
- *	
- * todo:
- *	- changing of values
- *	- WHILE/FOR/CASE implementation
- *	- Timer implementation
- *	- clean up code
- *	- optimizing code
+ *  - able to preprozess file (removes comments and stuff, no one needs that, right?)
+ *  - able to interpret simple VAR blocks (dynamic allocation with HashMaps, l33th4XX!)
+ *  - able to Print out stuff (yeah yeah i know, Structured Text has no PRINT function,
+ *                             and thats one of the many reasons why this Interpreter 
+ *                             is fucking awesome! why? cause it can interpret more than
+ *                             it should or could!)
+ *  - able to interpret IF conditions (pretty damn great, isnt it?)
  *
- * 
+ * todo:
+ *  - changing of values
+ *  - WHILE/FOR/CASE implementation
+ *  - Timer implementation
+ *  - clean up code
+ *  - optimizing code
+ *
+ *
  * this is the Main file, containing the main function to start the Interpreter.
  *
  * to get this to work you have to change the path to the structured text by hand for
@@ -34,33 +34,31 @@ import javax.script.ScriptEngineManager;
 
 class Icarus {
 
-
-	/* at first create a container to hold the VAR values */
-	public static Container container = new Container();
-	private static Boolean_Stack stack = new Boolean_Stack();
-	/*
-	 * convert given string so that the Java Script Engine can Interpret it,
-	 * will replace following matches: 
-	 * FROM		| TO
-	 * ---------+------------
-	 * '='		| ' == '
-	 * 'TRUE'	| ' true '
-	 * 'FALSE'	| ' false '
-	 * 'XOR'	| ' ^ '
-	 * 'AND'	| ' && '
-	 * 'NOT'	| ' !'
-	 * '&'		| ' && '
-	 * '<>'		| ' != '
-	 * 'MOD'	| ' % '
-	 * 'OR'		| ' || '
-	 * ---------+------------
-	 */ 
+    /* at first create a container to hold the VAR values */
+    public static Container container = new Container();
+    private static Boolean_Stack stack = new Boolean_Stack();
+    /*
+     * convert given string so that the Java Script Engine can Interpret it,
+     * will replace following matches: 
+     * FROM     | TO
+     * ---------+------------
+     * '='      | ' == '
+     * 'TRUE'   | ' true '
+     * 'FALSE'  | ' false '
+     * 'XOR'    | ' ^ '
+     * 'AND'    | ' && '
+     * 'NOT'    | ' !'
+     * '&'      | ' && '
+     * '<>'     | ' != '
+     * 'MOD'    | ' % '
+     * 'OR'     | ' || '
+     * ---------+------------
+     */
     public static String convert_condition(String code) {
         String final_condition = "";
-        
+
         int spot = 0;
-        
-    
+
         for( ; spot < code.length(); spot++ ) {
             if ( (code.charAt(spot) == '=') )
             {
@@ -75,7 +73,7 @@ class Icarus {
                 spot += 3;
                 continue;
             } else if ( (code.charAt(spot)     == 'F') &&
-                        (code.charAt(spot + 1) == 'A') && 
+                        (code.charAt(spot + 1) == 'A') &&
                         (code.charAt(spot + 2) == 'L') &&
                         (code.charAt(spot + 3) == 'S') &&
                         (code.charAt(spot + 4) == 'E') )
@@ -104,8 +102,8 @@ class Icarus {
                 final_condition += " !";
                 spot += 2;
                 continue;
-            } else if ( (code.charAt(spot) == '&') ) 
-            { 
+            } else if ( (code.charAt(spot) == '&') )
+            {
                 final_condition += " && ";
                 continue;
             } else if ( (code.charAt(spot)     == '<') &&
@@ -127,7 +125,7 @@ class Icarus {
                 final_condition += " || ";
                 spot += 1;
                 continue;
-            } 
+            }
             final_condition += code.charAt(spot);
         }
         return final_condition;
@@ -142,109 +140,109 @@ System.out.println(blub + "\n---------------------------------------------------
 System.out.println("preprozessed to:----------------------------------------------------");
 System.out.println(code + "\n-----------------------------------------------------------");
 
-		interpret(code);
+        interpret(code);
     }
 
-	public static void interpret (String string) throws Exception { 
+    public static void interpret (String string) throws Exception { 
 
         ScriptEngineManager factory = new ScriptEngineManager();
         ScriptEngine engine = factory.getEngineByName("JavaScript");
-        
+
         int INDEX = 0;
-		StringBuilder code = new StringBuilder(string);  
+        StringBuilder code = new StringBuilder(string);
 
         for( ;INDEX < code.length(); INDEX++) {
 
-            if ( code.charAt(INDEX) == '§' ) 
+            if ( code.charAt(INDEX) == '§' )
             {
                 continue;
             } else if ( (code.charAt(INDEX)     == 'I') &&
                         (code.charAt(INDEX + 1) == 'F') )
             {
-				INDEX += 2; 
-				StringBuilder condition = new StringBuilder("");
-				for(;;INDEX++) {
-					if ( (code.charAt(INDEX)	 == 'T') &&
-						 (code.charAt(INDEX + 1) == 'H') &&
-						 (code.charAt(INDEX + 2) == 'E') &&
-						 (code.charAt(INDEX + 3) == 'N') )
-					{
-						/*
-						 * cut function here, then recursive call of interpret function
-						 */
-						stack.push((Boolean)engine.eval(convert_condition(container.replace(condition.toString()))));
-					} else if (code.charAt(INDEX) == '§') {
-						continue;
-					}
-					condition.append(code.charAt(INDEX));
-				}
-			} else if ( (code.charAt(INDEX) ==     'E') &&
-						(code.charAt(INDEX + 1) == 'L') && 
-						(code.charAt(INDEX + 2) == 'S') && 
-						(code.charAt(INDEX + 3) == 'E') )
-			{
-				if(stack.get_first()) {
-					stack.pop();
-					return;
-				} else {
-						INDEX += 3;
-						continue;
-				}	
-			} else if ( (code.charAt(INDEX) ==     'E') &&
-						(code.charAt(INDEX + 1) == 'L') && 
-						(code.charAt(INDEX + 2) == 'S') && 
-						(code.charAt(INDEX + 3) == 'I') &&
-						(code.charAt(INDEX + 4) == 'F') ) 
-			{
-				if(stack.get_first()) {
-					stack.pop();
-					return;
-				} else {
-					INDEX += 5;
-					for(;;INDEX++) {
-						if ( (code.charAt(INDEX)	 == 'T') &&
-							 (code.charAt(INDEX + 1) == 'H') &&
-							 (code.charAt(INDEX + 2) == 'E') &&
-							 (code.charAt(INDEX + 3) == 'N') )
-						{
-							stack.pop();
-							stack.push((Boolean)engine.eval(convert_condition(container.replace(condition.toString()))));
-							break;
-						} else if (code.charAt(INDEX) == '§') {
-							continue;
-						}
-						condition.append(code.charAt(INDEX));
-					}
-				}
-			} else if ( (code.charAt(INDEX) ==     'T') &&
-						(code.charAt(INDEX + 1) == 'H') &&
-						(code.charAt(INDEX + 2) == 'E') &&
-						(code.charAt(INDEX + 3) == 'N') ) 
-			{
-				if(stack.get_first()) {
+                INDEX += 2;
+                StringBuilder condition = new StringBuilder("");
+                for(;;INDEX++) {
+                    if ( (code.charAt(INDEX)     == 'T') &&
+                         (code.charAt(INDEX + 1) == 'H') &&
+                         (code.charAt(INDEX + 2) == 'E') &&
+                         (code.charAt(INDEX + 3) == 'N') )
+                    {
+                        /*
+                         * cut function here, then recursive call of interpret function
+                         */
+                        stack.push((Boolean)engine.eval(convert_condition(container.replace(condition.toString()))));
+                    } else if (code.charAt(INDEX) == '§') {
+                        continue;
+                    }
+                    condition.append(code.charAt(INDEX));
+                }
+            } else if ( (code.charAt(INDEX) ==     'E') &&
+                        (code.charAt(INDEX + 1) == 'L') &&
+                        (code.charAt(INDEX + 2) == 'S') &&
+                        (code.charAt(INDEX + 3) == 'E') )
+            {
+                if(stack.get_first()) {
+                    stack.pop();
+                    return;
+                } else {
+                        INDEX += 3;
+                        continue;
+                }
+            } else if ( (code.charAt(INDEX) ==     'E') &&
+                        (code.charAt(INDEX + 1) == 'L') &&
+                        (code.charAt(INDEX + 2) == 'S') &&
+                        (code.charAt(INDEX + 3) == 'I') &&
+                        (code.charAt(INDEX + 4) == 'F') )
+            {
+                if(stack.get_first()) {
+                    stack.pop();
+                    return;
+                } else {
+                    INDEX += 5;
+                    for(;;INDEX++) {
+                        if ( (code.charAt(INDEX)     == 'T') &&
+                             (code.charAt(INDEX + 1) == 'H') &&
+                             (code.charAt(INDEX + 2) == 'E') &&
+                             (code.charAt(INDEX + 3) == 'N') )
+                        {
+                            stack.pop();
+                            stack.push((Boolean)engine.eval(convert_condition(container.replace(condition.toString()))));
+                            break;
+                        } else if (code.charAt(INDEX) == '§') {
+                            continue;
+                        }
+                        condition.append(code.charAt(INDEX));
+                    }
+                }
+        } else if ( (code.charAt(INDEX) ==     'T') &&
+                        (code.charAt(INDEX + 1) == 'H') &&
+                        (code.charAt(INDEX + 2) == 'E') &&
+                        (code.charAt(INDEX + 3) == 'N') )
+            {
+                if(stack.get_first()) {
 
-			}
+            }
             } else if ( (code.charAt(INDEX)      == 'V') &&
                         (code.charAt(INDEX + 1)  == 'A') &&
                         (code.charAt(INDEX + 2)  == 'R') )
-            {  
-                INDEX += 3;    
-				String var_line = "";
-				Boolean first = true;
+            {
+                INDEX += 3;
+                String var_line = "";
+                Boolean first = true;
                 for ( ;; INDEX++ ) {
-                    if(code.charAt(INDEX) == '§') 
+                    if(code.charAt(INDEX) == '§')
                     {
-						if(first) {
-							first = false;
-							continue;
-						} else {
-							container.add(var_line);
-							var_line = "";
-							continue;
-						}
-                    } else if ( (code.charAt(INDEX)      == 'E') && 
+                        if(first) {
+                            first = false;
+                            continue;
+                        } else {
+                            container.add(var_line);
+                            var_line = "";
+                            continue;
+                        }
+                    } else if ( (code.charAt(INDEX)      == 'E') &&
                                 (code.charAt(INDEX + 1)  == 'N') &&
-                                (code.charAt(INDEX + 2)  == 'D') && 
+                                (code.charAt(INDEX + 2)  == 'D') &&
                                 (code.charAt(INDEX + 3)  == '_') &&
                                 (code.charAt(INDEX + 4)  == 'V') &&
                                 (code.charAt(INDEX + 5)  == 'A') &&
@@ -253,30 +251,30 @@ System.out.println(code + "\n---------------------------------------------------
                         INDEX += 7;
                         break;
                     }
-					var_line += code.charAt(INDEX);
+                    var_line += code.charAt(INDEX);
                 }
-            
+
             } else if ( (code.charAt(INDEX)     == 'P') &&
                         (code.charAt(INDEX + 1) == 'R') &&
                         (code.charAt(INDEX + 2) == 'I') &&
-                        (code.charAt(INDEX + 3) == 'N') && 
-                        (code.charAt(INDEX + 4) == 'T') ) 
+                        (code.charAt(INDEX + 3) == 'N') &&
+                        (code.charAt(INDEX + 4) == 'T') )
             {
                 INDEX += 6;
-				String print = "";
-				for( ;; INDEX++ ) {
-					if ( (code.charAt(INDEX)     == ')') &&
-						 (code.charAt(INDEX + 1) == ';') ) 
-					{
-						INDEX += 1;
-						System.out.println("#####PRINT#######");
-						System.out.println(container.replace(print));
-						break;
-					} else {
-						print += code.charAt(INDEX);
-					}
-				}
-                
+                String print = "";
+                for( ;; INDEX++ ) {
+                    if ( (code.charAt(INDEX)     == ')') &&
+                         (code.charAt(INDEX + 1) == ';') )
+                    {
+                        INDEX += 1;
+                        System.out.println("#####PRINT#######");
+                        System.out.println(container.replace(print));
+                        break;
+                    } else {
+                        print += code.charAt(INDEX);
+                    }
+                }
+
             } else if ( (code.charAt(INDEX)      == 'E') &&
                         (code.charAt(INDEX + 1)  == 'N') &&
                         (code.charAt(INDEX + 2)  == 'D') &&
@@ -288,9 +286,9 @@ System.out.println(code + "\n---------------------------------------------------
                         (code.charAt(INDEX + 8)  == 'R') &&
                         (code.charAt(INDEX + 9)  == 'A') &&
                         (code.charAt(INDEX + 10) == 'M') )
-            {  
-                break; 
-            } 
+            {
+                break;
+            }
         }/* end main for loop   */
-	}
+    }
 }
