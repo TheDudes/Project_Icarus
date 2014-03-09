402ce74776f38da8d8fede05ca08f0bfa718819d
4pfel <mages.simon@googlemail.com>
2014-03-09 06:07:57 +0000

    parser in a good state

---
+++ apfel/src/parser/TYPES.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) 2014, HAW-Landshut
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package parser;
+
+import java.util.*;
+
+/**
+ * This class only creates objects of different types to return them.
+ *
+ * @author Simon Mages <mages.simon@googlemail.com>
+ * @version 1.0
+ */
+public class TYPES {
+    /**
+     * getType only need a String representing a type, like DINT
+     * it then creates a new Object of this type and returns it.
+     * @param type typeString like DINT, SINT, TIME ...
+     * @return Object of the right type
+     */
+    public static Object getType(String type) {
+	switch(type) {
+	case "BOOL":
+	    return new Boolean(false);
+	case "SINT":
+	    return new Byte((byte)0);
+	case "INT":
+	    return new Short((short)0);
+	case "DINT":
+	    return new Integer(0);
+	case "LINT":
+	    return new Long(0);
+	case "USINT": //throw some Exception, because no unsingend type in java
+	    break;
+	case "UINT": //throw some Exception, because no unsingend type in java
+	    break;
+	case "UDINT": //throw some Exception, because no unsingend type in java
+	    break;
+	case "ULINT": //throw some Exception, because no unsingend type in java
+	    break;
+	case "REAL":
+	    return new Float(0.0);
+	case "LREAL":
+	    return new Double(0.0);
+	case "TIME":
+	case "DATE":
+	case "TIME_OF_DAY":
+	case "TOD":
+	case "DATE_AND_TIME":
+	case "DT":
+	    return new Integer((int) (System.currentTimeMillis() / 1000L));
+	case "STRING":
+	case "WSTRING":
+	    return new String("");
+        case "BYTE":
+	    return new Boolean[8];
+	case "WORD":
+	    return new Boolean[16];
+	case "DWORD":
+	    return new Boolean[32];
+	case "LWORD":
+	    return new Boolean[64];
+	}
+	return new Object(); // ToDo: new types, return values from functions, an so on
+    }
+
+    /**
+     * getType only need a String representing a type, like DINT
+     * it then creates a new Object of this type and returns it.
+     * In this case it also fills the new Object with the given Value.
+     * @param type typeString like DINT, SINT, TIME ...
+     * @param value valueString like "2.3", "32323" ...
+     * @return Object of the right type with the new value
+     */
+    public static Object getType(String type, String value) {
+	switch(type) {
+	case "BOOL":
+	    return new Boolean(value);
+	case "SINT":
+	    return new Byte(value);
+	case "INT":
+	    return new Short(value);
+	case "DINT":
+	    return new Integer(value);
+	case "LINT":
+	    return new Long(value);
+	case "USINT": //throw some Exception, because no unsingend type in java
+	    break;
+	case "UINT": //throw some Exception, because no unsingend type in java
+	    break;
+	case "UDINT": //throw some Exception, because no unsingend type in java
+	    break;
+	case "ULINT": //throw some Exception, because no unsingend type in java
+	    break;
+	case "REAL":
+	    return new Float(value);
+	case "LREAL":
+	    return new Double(value);
+	case "TIME":
+	case "DATE":
+	case "TIME_OF_DAY":
+	case "TOD":
+	case "DATE_AND_TIME":
+	case "DT":
+	    return new Integer(value);
+	case "STRING":
+	case "WSTRING":
+	    return new String(value);
+        case "BYTE":
+	    return new Boolean[8];
+	case "WORD":
+	    return new Boolean[16];
+	case "DWORD":
+	    return new Boolean[32];
+	case "LWORD":
+	    return new Boolean[64];
+	}
+	return new Object(); // ToDo: new types, return values from functions, an so on
+    }
+}
+
