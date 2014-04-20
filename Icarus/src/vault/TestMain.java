9222ec6e55e262ac70dafe4d021246e3b0f12822
Vault <s-aweinb@fh-landshut.de>
2014-04-20 17:16:30 +0000

    +1

--- Icarus/src/vault/TestMain.java
+++ Icarus/src/vault/TestMain.java
@@ -1,26 +1,25 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2014, HAW-Landshut
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-
 package vault;
 
-
 /**
- *
- * @author vault
+ * TestClass to test the features
+ * @author Aleksej Weinberg <weinberg.aleksej@yahoo.de>
+ * @version x.x
  */
 public class TestMain {
-        
-    public static void main(String[] args) {
-        LogWriter logwriter = new LogWriter("/home/vault/programing/Logfile", 10);
-        logwriter.log("bleefsdfub", 3, "Blsdub");
-        logwriter.log("blufewb", 3, "Blusdb");
-        logwriter.log("blffwub", 3, "Blwfwub");
-        logwriter.log("bleesfub", 3, "fwefBlub");
-        logwriter.log("bluasdfb", 3, "Blsdfub");
-        logwriter.log("blufdasb", 3, "Blfewub");
-        logwriter.log("basdflub", 3, "Bluwefwfeb");
-    }
+
 }
