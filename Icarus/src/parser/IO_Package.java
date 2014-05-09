fdbe5ee93c906b4ba18f680e3ba01e9edd23ba0e
4pfel <mages.simon@googlemail.com>
2014-05-09 12:55:36 +0000

    added the right IO_Package ...

---
+++ Icarus/src/parser/IO_Package.java
@@ -0,0 +1,63 @@
+
+
+
+/*
+ * Copyright (c) 2014, HAW-Landshut
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package parser;
+
+/**
+ * This class is only for the communication with the IOInterface
+ *
+ * @author Simon Mages <mages.simon@googlemail.com>
+ * @version 1.0
+ */
+
+public class
+IO_Package
+{
+	/**
+	 * The variables this class holds
+	 */
+	public  int      device_id;
+	public  int      pin_id;
+	public  boolean  value;
+
+	/**
+	 * io_package need the device_id, the pin_id and the bool value for the PIN
+	 * 
+	 * @param int the device_id
+	 * @param int the pin_id
+	 * @param int the value
+	 */
+	public
+	IO_Package (int device_id, int pin_id, boolean value)
+	{
+		this.device_id  = device_id;
+		this.pin_id     = pin_id;
+		this.value      = value;
+	}
+}
+
+
+
+
+
+
+
+
+
+
