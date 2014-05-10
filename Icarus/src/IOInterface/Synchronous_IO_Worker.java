0f3353a3b19cc1a8007752c166e031d6442b7a8a
L1nc0ln <Lincoln-Park@gmx.de>
2014-05-10 13:17:54 +0000

    added the barebones synchronous_IO_Worker so that the SynchronousIO works

---
+++ Icarus/src/IOInterface/Synchronous_IO_Worker.java
@@ -0,0 +1,22 @@
+package IOInterface;
+import linc.Config_Reader;
+import vault.*;
+import parser.*;
+import java.net.*;
+
+
+
+public class Synchronous_IO_Worker implements Runnable{
+   
+   public Synchronous_IO_Worker(){
+
+   }
+
+    public Synchronous_IO_Worker(LogWriter logger, InfoCollector infoColl, Socket syncSocket){
+        
+    }
+
+   public void run(){
+
+   }
+}
