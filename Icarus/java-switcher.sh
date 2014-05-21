af3d0cb7399520553fff41287cd8942e5f4a460f
d4ryus <w.wackerbauer@yahoo.de>
2014-05-21 16:40:02 +0000

    updated java-switcher.sh layout

--- Icarus/java-switcher.sh
+++ Icarus/java-switcher.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env zsh
 #set -x
 
-
 if [[ `ps | grep $$ | awk '{ print $4 }'` != zsh ]]
 then
     echo "Error: zsh needed."
@@ -14,23 +13,23 @@ setVars()
     HOSTNAME=/usr/bin/hostname
     JAVA=/usr/bin/java
     JAVAC=/usr/bin/javac
-    
+
     case `$HOSTNAME` in
-	beelzebub)
-	    ORACLE=/usr/lib/jvm/jdk1.7.0_51/bin
-	    OPEN=/usr/lib/jvm/java-7-openjdk/bin
-	    IBM=/usr/lib/jvm/ibm-java-x86_64-71/bin
-	    ;;
-	d4ryus)
-	    ORACLE=/home/d4ryus/coding/gay_java/jdk1.8.0_05/bin
-	    OPEN=/usr/lib/jvm/java-7-openjdk/bin
-	    IBM=/path/to/java
-	    ;;
-	*)
-	    print "... "
-	    sleep 1
-	    print "Idiot!"
-	    ;;
+    beelzebub)
+        ORACLE=/usr/lib/jvm/jdk1.7.0_51/bin
+        OPEN=/usr/lib/jvm/java-7-openjdk/bin
+        IBM=/usr/lib/jvm/ibm-java-x86_64-71/bin
+        ;;
+    d4ryus)
+        ORACLE=/home/d4ryus/coding/gay_java/jdk1.8.0_05/bin
+        OPEN=/usr/lib/jvm/java-7-openjdk/bin
+        IBM=/path/to/java
+        ;;
+    *)
+        print "... "
+        sleep 1
+        print "Idiot!"
+        ;;
     esac
 }
 
@@ -38,21 +37,21 @@ setJDK()
 {
     if [[ ! -d "${1}" ]]
     then
-	print "No $2 JDK at ${1}"
-	exit 1
+        print "No $2 JDK at ${1}"
+        exit 1
     fi
     print "Activate $2 JDK"
     print -n "Root "
     su - -c zsh -c "\
-    rm $JAVA;\
-    rm $JAVAC;\
-    ln -s $1/java $JAVA;\
-    ln -s $1/javac $JAVAC;"
+        rm $JAVA;\
+        rm $JAVAC;\
+        ln -s $1/java $JAVA;\
+        ln -s $1/javac $JAVAC;"
     if [[ $? != 0 ]]
     then
-	print "Something went wrong ..."
+        print "Something went wrong ..."
     else
-	print "Succes!"
+        print "Succes!"
     fi
 }
 
@@ -76,16 +75,15 @@ setVars
 
 case $1 in
     Oracle|oracle)
-	setJDK $ORACLE Oracle
-	;;
+        setJDK $ORACLE Oracle
+        ;;
     Open|open)
-	setJDK $OPEN Open
-	;;
+        setJDK $OPEN Open
+        ;;
     IBM|Ibm|ibm)
-	setJDK $IBM IBM
-	;;
+        setJDK $IBM IBM
+        ;;
     *)
-	help
-	;;
+        help
+        ;;
 esac
-
